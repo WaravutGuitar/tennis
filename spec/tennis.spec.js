53be311066dc511a3376e8977816ad3fe522c2ce
Waravut Woravut <guitar15057@outlook.com>
2016-09-28 14:42:59 +0700

    test scoreA Love - Love

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -1,16 +1,30 @@
 function Tennis(){
+  var scoreA ;
+  var scoreB ;
   this.start = function() {
-
+      scoreA = 0;
+      scoreB = 0;
   };
-  this.echo = function() {
-    return 'Love - Love';
+
+  this.scoreA = function() {
+    if(scoreA === 0){
+      scoreA = 15;
+    }
   }
+  this.echo = function() {
+    if(scoreA === 0 && scoreB === 0){
+    return 'Love - Love';
+    }
+  }
+
 }
 
+
 describe('Tennis game', function(){
-  it('should echo "Love - Love" when starting the game', function(){
     var tennis = new Tennis();
+  it('should echo "Love - Love" when starting the game', function(){
     tennis.start();
     expect(tennis.echo()).toEqual('Love - Love');
   })
+
 })
